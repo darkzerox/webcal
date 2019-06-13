a78369bab22f134d04e6a3cf947e6e712e083c3d
Chittakorn <chittakorn@magnetolabs.com>
2019-06-13 16:54:28 +0700

    update

--- build/js/script.js
+++ build/js/script.js
@@ -1,91 +1,117 @@
-"use strict";
+'use strict';
 
 (function ($) {
+  $(document).ready(function () {    
 
-  $(document).ready(function () {
+    //social share
+    $("#share").jsSocials({
+      shares: ["email", "twitter", "facebook", "pinterest"]
+    });
+
+    // var title = $(document).prop("title");
     var global_select_packet = [];
     var sum = 0;
 
     $.ajax({
       url: "/src/db/price.json",
-      dataType: 'json',
+      dataType: "json",
       success: function success(jsonData) {
-
         var packet = jsonPath(jsonData, "$..packets");
-        var packethtml = '';
+        var packethtml = "";
 
         packet[0].forEach(function (res) {
           // console.log(res)
-          packethtml += "          \n          <div class=\"form-check\">\n            <input class=\"packet form-check-input\" type=\"radio\" name=\"packet-item\" id=\"" + res.packet + "\" val=\"" + res.packet + "\">\n            <label class=\"form-check-label\" for=\"" + res.packet + "\">\n              " + res.label + "\n            </label>\n          </div>\n\n          ";
+          packethtml += '    \n          <div class="col s3 packets">\n            <label class="card">                        \n              <input class="packet card-content" name="packet-item" type="radio" id="' + res.packet + '" val="' + res.packet + '" />\n              <span>' + res.label + '</span>               \n            </label>   \n    \n          </div>\n          ';
         });
-        packethtml = "<ul>" + packethtml + "</ul>";
+        packethtml = '<div>' + packethtml + '</div>';
 
-        $('#packet_section').html('').html(packethtml);
+        $("#packet_section").html("").html(packethtml);
 
-        $('.packet').click(function () {
+        $(".packet").click(function () {
+          var _this = this;
 
-          //reset total
-          sum = 0;
-          global_select_packet = [];
+          setTimeout(function () {
+            setPacket_from_Store();
+            $('.packet').parent().removeClass('active');
+            $(_this).parent().addClass('active');
+          }, 100);
 
-          var pk = $(this).attr('val');
+          var pk = $(this).attr("val");
+          //set packet local store
+
+          localStorage.setItem("packet", pk);
+          // console.log(pk)
+
           var packetData = jsonPath(jsonData, "$.." + pk);
-
           var packet_basic = jsonPath(packetData, "$..basic");
           var packet_ecomerce = jsonPath(packetData, "$..ecommerce");
           var packet_extra = jsonPath(packetData, "$..extra");
           var packet_server = jsonPath(packetData, "$..server");
 
-          getOptionData(packet_basic, 'packet_option', 'Basic Option');
-          getOptionData(packet_ecomerce, 'packet_ecommerce', 'Ecommerce Option');
-          getOptionData(packet_extra, 'packet_extra', 'Extra Option');
-          getOptionData(packet_server, 'packet_server', 'Server Option');
+          getOptionData(packet_basic, "packet_option", "Basic Option", "Lorem");
+
+          getOptionData(packet_ecomerce, "packet_ecommerce", "Ecommerce Option");
+          getOptionData(packet_extra, "packet_extra", "Extra Option");
+          getOptionData(packet_server, "packet_server", "Server Option");
           selectEvent();
         });
       }
     });
 
-    function getOptionData(data_list, el, title) {
-      var html_skele = "<h3>" + title + "</h3>";
-      $.each(data_list, function (i, obj) {
+    function getOptionData(data_list, el, title, desc) {
+      if (!desc) {
+        desc = '';
+      }
+      var html_skele = '<h5>' + title + '</h5><p>' + desc + '</p>';
+      var amount = "";
+      var cal_class = "col s6";
 
+      $.each(data_list, function (i, obj) {
         $.each(obj, function (key, val) {
           // console.log(val.option_dynamic)
-          var amount = '';
+          amount = '';
           if (val.option_dynamic == true) {
-            amount = "\n            <div class=\"input-group-append\">\n            <input type=\"number\" class=\"form-control item-amount\" >\n           </div>            \n            ";
+            amount = '<span class="range-field">\n              <input type="range" class="item-amount " placeholder="Input page amount" step="5" min="5" max="50"  />  \n              \n            </span>';
+            cal_class = "col s12";
+          } else {
+            cal_class = "col s6";
           }
-          html_skele += "\n          <div class=\"input-group\">\n            <div class=\"option-item custom-control custom-checkbox\">\n              <input type=\"checkbox\" class=\"custom-control-input option-val\" id=\"" + val.option_key + "\" value=\"" + val.option_price + "\">\n              <label class=\"custom-control-label\" for=\"" + val.option_key + "\">" + val.option_label + " : " + val.option_price + "</label>\n              " + amount + "\n            </div> \n            \n          </div>         \n      ";
+          html_skele += '          \n            <div class="option-item ' + cal_class + '">              \n              <label>\n              <input type="checkbox" class="custom-control-input option-val" id="' + val.option_key + '" value="' + val.option_price_start + '" val-end="' + val.option_price_end + '">\n              <span class="float-left item-title">' + val.option_label + '</span>\n              ' + amount + ' \n              </label>\n          </div>';
         });
       });
 
-      $('#' + el).html('').html(html_skele + '<hr/>');
+      $("#" + el).html(" ").html(html_skele);
 
+      // set_range_slide()
       amount_click();
     }
 
     function amount_click() {
-      $('.item-amount').change(function () {
+      $(".item-amount").change(function () {
         // console.log($(this).val())
-        $(this).parent().parent().find('.option-val').prop('checked', true);
+        $(this).parent().parent().find(".option-val").prop("checked", true);
       });
     }
 
     function selectEvent() {
-      $('.option-item').on('change', function (event) {
-
-        // console.log('change') 
-
-        var is_active = $(this).find('.option-val').prop("checked");
+      $(".option-item").on("change", function (event) {
+        var is_active = $(this).find(".option-val").prop("checked");
         // console.log(is_active)
-        var item_amount = $(this).find('.item-amount').val();
+        var item_amount = $(this).find(".item-amount").val();
+        if (!item_amount) {
+          item_amount = 1;
+        }
         // console.log('item_amount ' + item_amount)
 
-        var id = $(this).find('.option-val').attr('id');
-        var val = $(this).find('.option-val').val();
+        var id = $(this).find(".option-val").attr("id");
+        var val_start = $(this).find(".option-val").val();
+        var val_end = $(this).find(".option-val").attr('val-end');
 
-        if (item_amount > 0) {
-          val *= item_amount;
+        if (item_amount > 1) {
+          val_start *= item_amount;
+          val_end *= item_amount;
+          $(this).find('.item-title-amount').remove();
+          $(this).find('.item-title').append('<span class="item-title-amount">' + item_amount + '</span>');
         }
 
         global_select_packet = $.grep(global_select_packet, function (value) {
@@ -93,49 +119,139 @@
         });
 
         if (!is_active) {
-          $(this).removeClass('item-active');
+          $(this).removeClass("item-active");
         } else {
-          $(this).addClass('item-active');
+          $(this).addClass("item-active");
           global_select_packet.push({
             key: id,
-            price: val
+            price_start: val_start,
+            price_end: val_end,
+            item_amount: item_amount
           });
         }
-        // console.log(global_select_packet)
-        var before_count = $('#total').text();
-        var total = sum_select_packet(global_select_packet);
-        $('#total').text(total);
 
-        //animate counter
-        $('#total').each(function () {
-          var $this = $(this);
+        show_result(global_select_packet);
+        // console.log(global_select_packet);
+
+        //save packet data to local storeage
+        localStorage.setItem("packet_data", JSON.stringify(global_select_packet));
+
+        $("#total span").each(function () {
+          $this = $(this);
+          var before_count = $this.text();
+          var total = sum_select_packet(global_select_packet);
+          // console.log(total)
+          if ($this.hasClass('sum-start')) {
+            total = total.start;
+          } else {
+            total = total.end;
+          }
+          var $el = $this;
           $({
-            Counter: parseInt(before_count)
+            someValue: before_count
           }).animate({
-            Counter: $this.text()
+            someValue: total
           }, {
-            duration: 1500,
+            duration: 500,
             easing: 'swing',
             step: function step() {
-              $this.text(Math.ceil(this.Counter));
+              $el.text(commaSeparateNumber(Math.round(this.someValue)));
             },
             complete: function complete() {
-              $this.text(total);
+
+              $this.text(commaSeparateNumber(total));
             }
           });
         });
-
-        // console.log(total);
       });
     }
+  });
 
-    function sum_select_packet(obj) {
-      sum = 0;
-      $.each(obj, function (i, j) {
-        // console.log(j)
-        sum += parseInt(j.price);
-      });
-      return sum;
+  function sum_select_packet(obj) {
+    // console.log(obj)
+    var sum = [];
+    var start = 0;
+    var end = 0;
+    $.each(obj, function (i, j) {
+      start += parseInt(j.price_start);
+      end += parseInt(j.price_end);
+    });
+    sum.start = start;
+    sum.end = end;
+    return sum;
+  }
+
+  function commaSeparateNumber(val) {
+    while (/(\d+)(\d{3})/.test(val.toString())) {
+      val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
     }
+    return val;
+  }
+
+  $(window).on("load", function () {
+    //get save data from localstore
+    var local_packet = localStorage.getItem("packet");
+
+    if (local_packet != null) {
+      $("#packet_section").find("#" + local_packet).prop("checked", true).click();
+    }
+  });
+
+  function setPacket_from_Store() {
+    var local_packet_data = JSON.parse(localStorage.getItem("packet_data"));
+    // console.log(local_packet_data)
+    if (local_packet_data != null) {
+      $.each(local_packet_data, function (key, val) {
+        // console.log(val.key)
+        $("#" + val.key).click();
+      });
+    }
+  }
+
+  function show_result(data) {
+    data = data.sort();
+    var result_html = '';
+    var result_item_amount = '';
+    $.each(data, function (i, j) {
+      console.log(j);
+      if (j.item_amount > 1) {
+        result_item_amount = ' <div class="result_item_amount">X ' + j.item_amount + '</div>';
+      } else {
+        result_item_amount = '';
+      }
+      result_html += '\n      \n        <div class="row">\n          <div class="col s6">\n            <div class="result_name">' + j.key + '</div>\n            ' + result_item_amount + '\n          </div>\n          <div class="col s6 right-align">' + commaSeparateNumber(j.price_start) + ' ~ ' + commaSeparateNumber(j.price_end) + '</div>\n        </div>\n     \n      ';
+    });
+
+    $('#result').html(result_html);
+    // console.log(data)
+  }
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAf9Qmetk1pcnEoctwdNs1tfSfBB2cOLjs",
+    authDomain: "webcal-824b7.firebaseapp.com",
+    databaseURL: "https://webcal-824b7.firebaseio.com",
+    projectId: "webcal-824b7",
+    storageBucket: "webcal-824b7.appspot.com",
+    messagingSenderId: "1024371885166",
+    appId: "1:1024371885166:web:a6317657cf2207f2"
+  };
+  // Initialize Firebase
+  var app = firebase.initializeApp(firebaseConfig);
+  var db = app.firestore();
+  // let booksRef = db.ref('books');
+
+  $(".subscribe").click(function () {
+    db.collection("calDB").add({
+      email: $("#email").val(),
+      packet: localStorage.getItem("packet"),
+      data: localStorage.getItem("packet_data"),
+      date: new Date(),
+      total: $("#total").text()
+    }).then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    }).catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
   });
 })(window.jQuery);
--- src/js/script.js
+++ src/js/script.js
@@ -1,132 +1,161 @@
 (function ($) {
-
-
   $(document).ready(function () {
 
+   
 
 
     //social share
     $("#share").jsSocials({
-      shares: ["email", "twitter", "facebook", "pinterest", ]
+      shares: ["email", "twitter", "facebook", "pinterest"]
     });
 
-    var title = $(document).prop('title');
-    var global_select_packet = []
-    var sum = 0
+    // var title = $(document).prop("title");
+    var global_select_packet = [];
+    var sum = 0;
 
     $.ajax({
       url: "/src/db/price.json",
-      dataType: 'json',
+      dataType: "json",
       success: function (jsonData) {
-
         var packet = jsonPath(jsonData, "$..packets");
-        var packethtml = ''
+        var packethtml = "";
 
-        packet[0].forEach(res => {
+        packet[0].forEach((res) => {
           // console.log(res)
           packethtml += `    
-          <p>
-            <label>
-              <input class="packet" name="packet-item" type="radio" id="${res.packet}" val="${res.packet}" />
-              <span>${res.label}</span>
-            </label>      
-          </p>
-          `
+          <div class="col s3 packets">
+            <label class="card">                        
+              <input class="packet card-content" name="packet-item" type="radio" id="${
+                res.packet
+              }" val="${res.packet}" />
+              <span>${res.label}</span>               
+            </label>   
+    
+          </div>
+          `;
         });
         packethtml = `<div>${packethtml}</div>`;
 
-        $('#packet_section').html('').html(packethtml)
+        $("#packet_section")
+          .html("")
+          .html(packethtml);
 
-        $('.packet').click(function () {
-
+        $(".packet").click(function () {
           setTimeout(() => {
-            setPacket_from_Store()
+            setPacket_from_Store();
+            $('.packet').parent().removeClass('active')
+            $(this).parent().addClass('active')
+
+
           }, 100);
 
-          var pk = $(this).attr('val')
+          var pk = $(this).attr("val");
           //set packet local store
 
-          localStorage.setItem('packet', pk)
+          localStorage.setItem("packet", pk);
           // console.log(pk)
 
-
-
           var packetData = jsonPath(jsonData, "$.." + pk);
-
           var packet_basic = jsonPath(packetData, "$..basic");
           var packet_ecomerce = jsonPath(packetData, "$..ecommerce");
           var packet_extra = jsonPath(packetData, "$..extra");
           var packet_server = jsonPath(packetData, "$..server");
 
-          getOptionData(packet_basic, 'packet_option', 'Basic Option')
-          getOptionData(packet_ecomerce, 'packet_ecommerce', 'Ecommerce Option')
-          getOptionData(packet_extra, 'packet_extra', 'Extra Option')
-          getOptionData(packet_server, 'packet_server', 'Server Option')
-          selectEvent()
-        })
-      },
+          getOptionData(packet_basic, "packet_option", "Basic Option", "Lorem");
+
+          getOptionData(packet_ecomerce, "packet_ecommerce", "Ecommerce Option");
+          getOptionData(packet_extra, "packet_extra", "Extra Option");
+          getOptionData(packet_server, "packet_server", "Server Option");
+          selectEvent();
+        });
+      }
     });
 
+    function getOptionData(data_list, el, title, desc) {
+      if (!desc) {
+        desc = ''
+      }
+      let html_skele = `<h5>${title}</h5><p>${desc}</p>`;
+      var amount = "";
+      var cal_class = "col s6";
 
-    function getOptionData(data_list, el, title) {
-      let html_skele = `<h3>${title}</h3>`;
       $.each(data_list, function (i, obj) {
         $.each(obj, function (key, val) {
           // console.log(val.option_dynamic)
-          var amount = ''
+          amount = ''
           if (val.option_dynamic == true) {
-            amount = `
-                      
-            <p class="range-field">
-              <input type="number" class="item-amount" placeholder="Input page amount" />
-             
-            </p>
+            amount = `<span class="range-field">
+              <input type="range" class="item-amount " placeholder="Input page amount" step="5" min="5" max="50"  />  
+              
+            </span>`;
+            cal_class = "col s12"
 
-            `
+          } else {
+            cal_class = "col s6";
           }
-          html_skele += `
-          
-            <div class="option-item ">              
+          html_skele += `          
+            <div class="option-item ${cal_class}">              
               <label>
-              <input type="checkbox" class="custom-control-input option-val" id="${val.option_key}" value="${val.option_price}" >
-              <span>${val.option_label} : ${val.option_price}</span>
+              <input type="checkbox" class="custom-control-input option-val" id="${
+                val.option_key
+              }" value="${val.option_price_start}" val-end="${val.option_price_end}">
+              <span class="float-left item-title">${val.option_label}</span>
+              ${amount} 
               </label>
-              ${amount}      
-          </div>
-      `
+          </div>`;
         });
       });
 
-      $('#' + el).html('').html(html_skele)
+      $("#" + el)
+        .html(" ")
+        .html(html_skele);
 
-
-      amount_click()
-
-
-
+      // set_range_slide()
+      amount_click();
 
     }
 
+
     function amount_click() {
-      $('.item-amount').change(function () {
+      $(".item-amount").change(function () {
         // console.log($(this).val())
-        $(this).parent().parent().find('.option-val').prop('checked', true)
-      })
+        $(this)
+          .parent()
+          .parent()
+          .find(".option-val")
+          .prop("checked", true);
+      });
     }
 
     function selectEvent() {
-      $('.option-item').on('change', function (event) {
-
-        let is_active = $(this).find('.option-val').prop("checked");
+      $(".option-item").on("change", function (event) {
+        let is_active = $(this)
+          .find(".option-val")
+          .prop("checked");
         // console.log(is_active)
-        let item_amount = $(this).find('.item-amount').val();
+        let item_amount = $(this)
+          .find(".item-amount")
+          .val();
+        if (!item_amount) {
+          item_amount = 1
+        }
         // console.log('item_amount ' + item_amount)
 
-        let id = $(this).find('.option-val').attr('id')
-        let val = $(this).find('.option-val').val()
+        let id = $(this)
+          .find(".option-val")
+          .attr("id");
+        let val_start = $(this)
+          .find(".option-val")
+          .val();
+        let val_end = $(this)
+          .find(".option-val")
+          .attr('val-end');
 
-        if (item_amount > 0) {
-          val *= item_amount
+        if (item_amount > 1) {
+          val_start *= item_amount;
+          val_end *= item_amount;
+          $(this).find('.item-title-amount').remove()
+          $(this).find('.item-title').append(`<span class="item-title-amount">${item_amount}</span>`)
         }
 
         global_select_packet = $.grep(global_select_packet, function (value) {
@@ -134,101 +163,166 @@
         });
 
         if (!is_active) {
-          $(this).removeClass('item-active')
+          $(this).removeClass("item-active");
         } else {
-          $(this).addClass('item-active')
+          $(this).addClass("item-active");
           global_select_packet.push({
             key: id,
-            price: val
-          })
+            price_start: val_start,
+            price_end: val_end,
+            item_amount: item_amount
+          });
         }
-        console.log(global_select_packet)
+
+        show_result(global_select_packet)
+        // console.log(global_select_packet);
 
         //save packet data to local storeage
-        localStorage.setItem('packet_data', JSON.stringify(global_select_packet))
+        localStorage.setItem(
+          "packet_data",
+          JSON.stringify(global_select_packet)
+        );
 
-        let before_count = $('#total').text()
-        let total = sum_select_packet(global_select_packet)
-        $('#total').text(total)
-
-        //animate counter
-        $('#total').each(function () {
-          var $this = $(this);
+        $("#total span").each(function () {
+          $this = $(this)
+          let before_count = $this.text();
+          let total = sum_select_packet(global_select_packet);
+          // console.log(total)
+          if ($this.hasClass('sum-start')) {
+            total = total.start
+          } else {
+            total = total.end
+          }
+          var $el = $this;
           $({
-            Counter: parseInt(before_count)
+            someValue: before_count
           }).animate({
-            Counter: $this.text()
+            someValue: total
           }, {
-            duration: 1500,
+            duration: 500,
             easing: 'swing',
             step: function () {
-              $this.text(Math.ceil(this.Counter));
+              $el.text(commaSeparateNumber(Math.round(this.someValue)));
             },
             complete: function () {
-              $this.text(total)
+
+              $this.text(commaSeparateNumber(total));
             }
-          });
-        });
-
-        // console.log(total);
-
-      })
+          })
 
 
+        })
+
+
+      });
     }
 
-
-
-    function sum_select_packet(obj) {
-      sum = 0;
-      $.each(obj, function (i, j) {
-        // console.log(j)
-        sum += parseInt(j.price);
-
-      })
-
-      $(document).prop('title', title + " ( cost total : " + sum + " )");
-
-
-
-      return sum
-    }
-
-
-
-
-
-
-
-  })
-
-
-  $(window).on('load', function () {
-    //get save data from localstore
-    let local_packet = localStorage.getItem('packet');
-
-
-    if (local_packet != null) {
-      $('#packet_section').find('#' + local_packet).prop('checked', true).click()
-    }
 
 
   });
 
+  function sum_select_packet(obj) {
+    // console.log(obj)
+    let sum = []
+    let start = 0
+    let end = 0
+    $.each(obj, function (i, j) {
+      start += parseInt(j.price_start);
+      end += parseInt(j.price_end);
+    });
+    sum.start = start
+    sum.end = end
+    return sum;
+  }
 
+  function commaSeparateNumber(val) {
+    while (/(\d+)(\d{3})/.test(val.toString())) {
+      val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    }
+    return val;
+  }
+
+  $(window).on("load", function () {
+    //get save data from localstore
+    let local_packet = localStorage.getItem("packet");
+
+    if (local_packet != null) {
+      $("#packet_section")
+        .find("#" + local_packet)
+        .prop("checked", true)
+        .click();
+    }
+  });
 
   function setPacket_from_Store() {
-    let local_packet_data = JSON.parse(localStorage.getItem('packet_data'))
+    let local_packet_data = JSON.parse(localStorage.getItem("packet_data"));
     // console.log(local_packet_data)
     if (local_packet_data != null) {
       $.each(local_packet_data, function (key, val) {
         // console.log(val.key)
-        $('#' + val.key).click()
-      })
+        $("#" + val.key).click();
+      });
     }
+  }
 
+  function show_result(data) {
+    data = data.sort()
+    let result_html = ''
+    let result_item_amount = ''
+    $.each(data, function (i, j) {
+      console.log(j)
+      if (j.item_amount > 1) {
+        result_item_amount = ` <div class="result_item_amount">X ${j.item_amount}</div>`
+      } else {
+        result_item_amount = ''
+      }
+      result_html += `
+      
+        <div class="row">
+          <div class="col s6">
+            <div class="result_name">${j.key}</div>
+            ${result_item_amount}
+          </div>
+          <div class="col s6 right-align">${commaSeparateNumber(j.price_start)} ~ ${commaSeparateNumber(j.price_end)}</div>
+        </div>
+     
+      `
+    });
+
+    $('#result').html(result_html);
+    // console.log(data)
 
   }
 
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAf9Qmetk1pcnEoctwdNs1tfSfBB2cOLjs",
+    authDomain: "webcal-824b7.firebaseapp.com",
+    databaseURL: "https://webcal-824b7.firebaseio.com",
+    projectId: "webcal-824b7",
+    storageBucket: "webcal-824b7.appspot.com",
+    messagingSenderId: "1024371885166",
+    appId: "1:1024371885166:web:a6317657cf2207f2"
+  };
+  // Initialize Firebase
+  let app = firebase.initializeApp(firebaseConfig);
+  let db = app.firestore();
+  // let booksRef = db.ref('books');
 
+  $(".subscribe").click(function () {
+    db.collection("calDB")
+      .add({
+        email: $("#email").val(),
+        packet: localStorage.getItem("packet"),
+        data: localStorage.getItem("packet_data"),
+        date: new Date(),
+        total: $("#total").text()
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  });
 })(window.jQuery);
